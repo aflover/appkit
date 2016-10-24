2b2cc85094b06d772de99055fc6a5058d1638663
jetiny <86287344@qq.com>
2016-10-25 00:03:39 +0800

    modify

--- src/components/SimpleTab.vue
+++ src/components/SimpleTab.vue
@@ -3,7 +3,7 @@
 		<div :class="classes.concat(['tab-nav'])">
 			<div 
 				v-for="it in tabs"
-				:class="['tab-title', (it.value == value) && 'is-selected']" 
+				:class="['tab-title', (it.value == value) && 'is-selected', it.titleTheme]" 
 				@mouseenter="mouse && $emit('input', it.value)"
 				@click="$emit('input', it.value)">
 				{{it.title}}
@@ -12,7 +12,7 @@
 		<div :class="['tab-container']">
 			<div
 				v-for="it in tabs"
-				:class="['tab-content', (it.value == value) && 'is-selected']"
+				:class="['tab-content', (it.value == value) && 'is-selected', it.contentTheme]"
 				>
 				<slot :name="it.value" ></slot>
 			</div>
--- src/demo/src/DemoTab.vue
+++ src/demo/src/DemoTab.vue
@@ -7,27 +7,61 @@
 				bordered &&'is-bordered', 
 				flex &&'is-flex', 
 				]">
-			<div slot="label1">
+			<ul slot="label1">
 				选项
 				<check-box v-model="offseted">is-offseted</check-box>
 				<check-box v-model="titled">is-titled</check-box>
 				<check-box v-model="bordered">is-bordered</check-box>
 				<check-box v-model="flex">is-flex</check-box>
-			</div>
-			<div slot="label2">内容区2</div>
+			</ul>
+			<ul class="group-col" slot="label2">
+				<li class="group-item" v-for="i in 5">list-item-{{i}}</li>
+			</ul>
 			<div slot="label3">内容区3</div>
 			<div slot="label4">内容区4</div>
 		</simple-tab>
+		<br>
+		<br>
+		<dropdown :open="openDrop">
+			<btn @click="openDrop=!openDrop">dropdown</btn>
+			<simple-tab style="width:300px" :tabs="tabs" v-model="tab" :class="['dropdown-view']" :classes="[
+				offseted &&'is-offseted', 
+				titled &&'is-titled', 
+				bordered &&'is-bordered', 
+				flex &&'is-flex', 
+				]">
+				<ul class="group-col" slot="label1">
+					<li class="group-item" v-for="i in 20">list-item-{{i}}</li>
+				</ul>
+				<ul class="group-col" slot="label2">
+					<li class="group-item" v-for="i in 10">list-item-{{i}}</li>
+				</ul>
+				<div slot="label3">内容区3</div>
+				<div slot="label4">内容区4</div>
+			</simple-tab>
+		</dropdown>
 	</div>
 </template>
+
+<style type="text/css">
+	
+	.tab-content.content-list {
+		padding: 0;
+	}
+	.tab-content.content-list > ul {
+		display:block; 
+		max-height: 200px; 
+		overflow:auto;
+	}
+</style>
 
 <script type="text/javascript">
 	export default {
 		data: function () {
 			return {
 				tabs: [
-					{ title:'标签1', value: "label1"},
-					{ title:'标签2', value: "label2"},
+					{ title:'标签1', value: "label1", contentTheme: 'content-list'},
+					{ title:'标签2', value: "label2", contentTheme: 'content-list'},
 					{ title:'标签3', value: "label3"},
 					{ title:'标签4', value: "label4"},
 				],
@@ -36,6 +70,8 @@
 				titled: true,
 				bordered: true,
 				flex: false,
+
+				openDrop: false,
 			};
 		}
 	}
